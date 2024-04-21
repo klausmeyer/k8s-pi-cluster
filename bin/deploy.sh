7ecbd939c580c0899a5126a04399e80b1aee539d
Klaus Meyer <spam@klaus-meyer.net>
2024-04-21 15:35:06 +0200

    add keycloak

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,9 +7,9 @@ set -u
 cd "./deployments/${CHART}"
 
 if [ -f "./Chart.lock" ]; then
-  yq --indent 0 '.dependencies | map(["helm", "repo", "add", .name, .repository] | join(" ")) | .[]' "./Chart.lock"  | sh --;
+  yq -r --indent 0 '.dependencies | map(["helm", "repo", "add", .name, .repository] | join(" ")) | .[]' "./Chart.lock"  | sh --;
 fi
 
 helm dependency build
 helm template --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
-helm upgrade --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
+helm upgrade --install --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
