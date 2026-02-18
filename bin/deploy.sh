d22711d8f78c0a16426fb36bb6a7aa4787944576
Klaus Meyer <spam@klaus-meyer.net>
2026-02-18 20:19:09 +0100

    debug

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
-set -u
+# set -e
+# set -o pipefail
+# set -u
 
-NAMESPACE="${NAMESPACE:-$1}"
-CHART="${CHART:-$1}"
-RELEASE="${RELEASE:-$1}"
+# NAMESPACE="${NAMESPACE:-$1}"
+# CHART="${CHART:-$1}"
+# RELEASE="${RELEASE:-$1}"
 
-cd "./deployments/${CHART}"
+# cd "./deployments/${CHART}"
 
-if [ -f "./Chart.lock" ]; then
-  yq -r --indent 0 '.dependencies | filter(.repository != "") | filter(.repository != "oci://*") | map(["helm", "repo", "add", "--force-update", .name, .repository] | join(" ")) | .[]' "./Chart.lock"  | sh --;
-fi
+# if [ -f "./Chart.lock" ]; then
+#   yq -r --indent 0 '.dependencies | filter(.repository != "") | filter(.repository != "oci://*") | map(["helm", "repo", "add", "--force-update", .name, .repository] | join(" ")) | .[]' "./Chart.lock"  | sh --;
+# fi
 
-helm dependency build
-helm template --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
-helm upgrade --install --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
+# helm dependency build
+# helm template --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
+# helm upgrade --install --create-namespace -n "${NAMESPACE}" "${RELEASE}" .
+
+pwd
+ls -l /root
+ls -l /root/.kube
+ls -l /root/.kube/config
+
+kubectl get nodes
